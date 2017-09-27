2d4cdc2c718b30d9db18399501af421fdb5bf56e
Zander Janse van Rensburg <zjvren@geevcookie.com>
2017-09-27 04:21:24 +0200

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #0072C6;
+                color: #fff;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #fff;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .server {
+                font-size: 20px;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    <img src="/logo.png" />
+                </div>
+
+                <div class="m-b-md">
+                    <p class="server">Server = {{ $ip }}</p>
+                </div>
+
+                <div class="links">
+                    <a href="https://azure.microsoft.com/en-us/free/" target="_blank">Sign Up</a>
+                    <a href="https://portal.azure.com" target="_blank">Azure Portal</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
